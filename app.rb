e8978f7df1c6201f55ce3570d3754e6d7ef04292
Julian George <jules104@gmail.com>
2019-09-05 16:01:38 +0100

    add get attack

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra/base'
-require 'player'
+require './lib/player'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -24,7 +24,9 @@ class Battle < Sinatra::Base
   end
 
   get '/attack' do
-    'Player1 attacked Player2'
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
+    erb:attack
   end
   # start the server if ruby file executed directly
   run! if app_file == $0
