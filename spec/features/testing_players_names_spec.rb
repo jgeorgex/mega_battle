471261046707330d9ad27e32b65cd20a98f53947
Julian George <jules104@gmail.com>
2019-09-05 14:05:33 +0100

    Add web_helpers folder

--- spec/features/testing_players_names_spec.rb
+++ spec/features/testing_players_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'test that expects playerts to fill in their names in a form' do
   scenario 'new player enters name in form' do
-    visit('/')
-    fill_in :player_1_name, with: 'Dave'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Dave vs. Mittens'
   end
 end
